ab0ed70bad293a474ab3d202cfe021d604b6512f
Boukezzoula <dzarmy25@gmail.com>
2022-09-03 07:27:42 -0400

    upload-images

--- App.tsx
+++ App.tsx
@@ -1,11 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import React from "react";
+import { TouchableOpacity } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome5";
 
 export default function App() {
+  const openImagePickerAsync = async () => {
+    const permissionResult =
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required");
+      return;
+    }
+    const pickerResult = await ImagePicker.launchImageLibraryAsync();
+    if (!pickerResult.cancelled) {
+      // keep state
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableOpacity
+        style={styles.uploadContainer}
+        onPress={() => {
+          openImagePickerAsync();
+        }}
+      >
+        <Icon
+          name="camera"
+          color="#617D96"
+          size={24}
+          style={{ alignSelf: "center" }}
+        />
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +41,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  uploadContainer: {
+    width: 100,
+    height: 100,
+    borderColor: "#617D96",
+    borderWidth: 2,
+    margin: 10,
+    justifyContent: "center",
+    alignSelf: "center",
+    borderStyle: "dashed",
+    borderRadius: 8,
   },
 });
